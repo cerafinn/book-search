bf288a2d03933eefcf776bb0ed15b127b71bf823
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-12 11:47:08 -0400

    setting up Apollo server

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,29 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+
+  }
+
+  type Book {
+
+  }
+
+  type Auth {
+
+  }
+
+  input NewBook {
+
+  }
+
+  type Query {
+
+  }
+
+  type Mutation {
+    
+  }
+`
+
+module.exports = typeDefs;
