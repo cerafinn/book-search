29a5f8e0fe5ad9378eeef134754dfab43fc76666
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-12 15:47:37 -0400

    corrected misspellings in both resolvers and loginform files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,7 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await (await User.findOne({ _id: context.user._id })).isSelected('-__v -password');
+        const userData = await (await User.findOne({ _id: context.user._id })).select('-__v -password');
         return userData;
       }
       throw new AuthenticationError('You are not logged in')
