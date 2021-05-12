a2f858e567ed8fe19f03f4415845555e29ca3a42
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-12 11:51:15 -0400

    working on typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,15 +2,25 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-
+    _id: ID!
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
   }
 
   type Book {
-
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: 
+    link: String
   }
 
   type Auth {
-
+    token: ID!
+    user: User
   }
 
   input NewBook {
@@ -18,11 +28,14 @@ const typeDefs = gql`
   }
 
   type Query {
-
+    me: User
   }
 
   type Mutation {
-    
+    login
+    addUser
+    saveBook
+    removeBook
   }
 `
 
